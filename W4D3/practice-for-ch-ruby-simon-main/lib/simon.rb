4e897dc24ea9185b1c9fe876036f468f9ed2be96
Elliot Chang <elliotchang126@gmail.com>
2023-06-13 21:31:57 -0700

    Actually works in pry

--- W4D3/practice-for-ch-ruby-simon-main/lib/simon.rb
+++ W4D3/practice-for-ch-ruby-simon-main/lib/simon.rb
@@ -10,10 +10,11 @@ class Simon
   end
 
   def play
-    until game_over
+    until @game_over
       take_turn
       system("clear")
     end
+    
     game_over_message
     reset_game
   end
@@ -21,7 +22,8 @@ class Simon
   def take_turn
     show_sequence
     require_sequence
-    until game_over
+
+    unless @game_over
       round_success_message
       @sequence_length += 1
     end
@@ -29,15 +31,16 @@ class Simon
 
   def show_sequence
     add_random_color
-    seq.each do |color|
+    @seq.each do |color|
       puts color
       sleep 1
       system("clear")
-      sleep 1
+      sleep 0.5
     end
   end
 
   def require_sequence
+    puts "Repeat the sequence by entering the first letter of each color on a new line."
     @seq.each do |color|
       input = gets.chomp
       if color[0] != input
