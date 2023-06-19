732bad2a40165cbb8809bfd73ed6344d4a44f1b3
Elliot Chang <elliotchang126@gmail.com>
2023-06-18 23:58:11 -0700

    Added add and show methods

--- W5D1/lru_cache.rb
+++ W5D1/lru_cache.rb
@@ -10,13 +10,21 @@ class LRUCache
     end
 
     def add(el)
-        @cache << el
+        if @cache.include?(el)
+            @cache.delete(el)
+            @cache << el
+        elsif @cache.count >= size
+            @cache.shift
+            @cache << el
+        else
+            @cache << el
+        end
     end
 
     def show
-
+        puts @cache
     end
 
     private
-    
+
 end
