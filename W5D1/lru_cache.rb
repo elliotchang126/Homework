a9afd5cdb5530b829f6746ce458c4997a5fedacf
Elliot Chang <elliotchang126@gmail.com>
2023-06-18 23:56:20 -0700

    Initialize and count

--- W5D1/lru_cache.rb
+++ W5D1/lru_cache.rb
@@ -1,25 +1,22 @@
 class LRUCache
 
-    def initialize
-
+    def initialize(num)
+        @cache = []
+        @size = num
     end
 
     def count
-
+        @cache.count
     end
 
     def add(el)
-
+        @cache << el
     end
 
     def show
 
     end
 
-
     private
-
-
-
-
+    
 end
