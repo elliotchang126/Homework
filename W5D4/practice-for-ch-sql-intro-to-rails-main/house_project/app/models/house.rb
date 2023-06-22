8eee557f834e759cd48655a4854a01ff9ffaad2c
Elliot Chang <elliotchang126@gmail.com>
2023-06-21 22:03:10 -0700

    Set up migration and models, getting error number expected

--- W5D4/practice-for-ch-sql-intro-to-rails-main/house_project/app/models/house.rb
+++ W5D4/practice-for-ch-sql-intro-to-rails-main/house_project/app/models/house.rb
@@ -2,4 +2,9 @@
 
 class House < ApplicationRecord
     validates :address, presence: true
+
+    has_many :tenants,
+        primary_key: :id,
+        foreign_key: :house_id,
+        class_name: :Person
 end
