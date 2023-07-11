539dd5e9ae30045f9da82caf4ae4c32dd38a89c9
Elliot Chang <elliotchang126@gmail.com>
2023-07-10 22:42:27 -0700

    Completed W8D2 hw

--- W8D2/practice-for-ch-js-intro-js-problems1-main/part-1.js
+++ W8D2/practice-for-ch-js-intro-js-problems1-main/part-1.js
@@ -1,64 +1,64 @@
-// function mysteryScoping1() {
-//     var x = 'out of block';
-//     if (true) {
-//       var x = 'in block';  
-//       console.log(x);
-//     }
-//     console.log(x);
-//   }
+function mysteryScoping1() {
+    var x = 'out of block';
+    if (true) {
+      var x = 'in block';  
+      console.log(x);
+    }
+    console.log(x);
+  }
 
-//   mysteryScoping1();
+  mysteryScoping1();
 
-//   function mysteryScoping2() {
-//     const x = 'out of block';
-//     if (true) {
-//       const x = 'in block';  
-//       console.log(x);
-//     }
-//     console.log(x);
-//   }
+  function mysteryScoping2() {
+    const x = 'out of block';
+    if (true) {
+      const x = 'in block';  
+      console.log(x);
+    }
+    console.log(x);
+  }
   
-//   mysteryScoping2();
+  mysteryScoping2();
 
-//   function mysteryScoping3() {
-//     const x = 'out of block';
-//     if (true) {
-//       var x = 'in block';  
-//       console.log(x);
-//     }
-//     console.log(x);
-//   }
+  function mysteryScoping3() {
+    const x = 'out of block';
+    if (true) {
+      var x = 'in block';  
+      console.log(x);
+    }
+    console.log(x);
+  }
   
-//   mysteryScoping3();
+  mysteryScoping3();
 
-//   function mysteryScoping4() {
-//     let x = 'out of block';
-//     if (true) {
-//       let x = 'in block';  
-//       console.log(x);
-//     }
-//     console.log(x);
-//   }
+  function mysteryScoping4() {
+    let x = 'out of block';
+    if (true) {
+      let x = 'in block';  
+      console.log(x);
+    }
+    console.log(x);
+  }
   
-//   mysteryScoping4();
+  mysteryScoping4();
 
-//   function mysteryScoping5() {
-//     let x = 'out of block';
-//     if (true) {
-//       let x = 'in block';  
-//       console.log(x);
-//     }
-//     let x = 'out of block again';
-//     console.log(x);
-// }
+  function mysteryScoping5() {
+    let x = 'out of block';
+    if (true) {
+      let x = 'in block';  
+      console.log(x);
+    }
+    let x = 'out of block again';
+    console.log(x);
+}
 
-// mysteryScoping5();
+mysteryScoping5();
 
-// function madLib(verb, adj, noun) {
-//     console.log(`We shall ${verb.toUpperCase()} the ${adj.toUpperCase()} ${noun.toUpperCase()}.`);
-// }
+function madLib(verb, adj, noun) {
+    console.log(`We shall ${verb.toUpperCase()} the ${adj.toUpperCase()} ${noun.toUpperCase()}.`);
+}
 
-// madLib('make', 'best', 'guac');
+madLib('make', 'best', 'guac');
 
 function isSubstring(phrase, sub) {
     return phrase.includes(sub);
@@ -87,5 +87,15 @@ function isPrime(num) {
 
 function sumOfNPrimes(n) {
     let sum = 0
-    
+    let count = 0
+    let i = 2
+
+    while (count < n) {
+        if (isPrime(i)) {
+            sum += i;
+            count++;
+        }
+        i++;
+    }
+    return sum;
 }
