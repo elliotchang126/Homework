7132a8026f4cdac162adeb53fda2ebd84a588f9e
Elliot Chang <elliotchang126@gmail.com>
2023-07-12 20:48:41 -0700

    Completed intro to canvas

--- W8D4/practice-for-ch-js-intro-to-canvas-main/canvas-practice.js
+++ W8D4/practice-for-ch-js-intro-to-canvas-main/canvas-practice.js
@@ -1,3 +1,19 @@
 document.addEventListener("DOMContentLoaded", function() {
   // Your code here
+  const canvasEl = document.getElementById("myCanvas");
+    canvasEl.width = 500;
+    canvasEl.height = 500;
+
+    const ctx = canvasEl.getContext("2d");
+    ctx.fillStyle = "red";
+    ctx.fillRect(0, 0, 500, 500);
+
+
+    ctx.beginPath();
+    ctx.arc(100, 100, 20, 0, 2*Math.PI, true);
+    ctx.strokeStyle = "blue";
+    ctx.lineWidth = 5;
+    ctx.stroke();
+    ctx.fillStyle = "green";
+    ctx.fill();
 });
